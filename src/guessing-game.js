38f514505765ba92d04ac237a3ba80b94da50de2
Protanton <protanton@gmail.com>
2019-10-10 19:15:34 +0300

    feat: guessing-game task solve

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min_number = 0;
+        this.max_number = 0;
+        this.guess_number = 0;
+    }
 
     setRange(min, max) {
-
+        this.min_number = min;
+        this.max_number = max;
     }
 
     guess() {
-
+        this.guess_number = Math.round((this.min_number + this.max_number) / 2);
+        return this.guess_number;
     }
 
     lower() {
-
+        this.max_number = this.guess_number;
     }
 
     greater() {
-
+        this.min_number = this.guess_number;
     }
 }
 
